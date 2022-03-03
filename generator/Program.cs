2d983cab01ac1f9334e1b8f3f25075c1d34700d3
Usman995 <57040341+Usman995@users.noreply.github.com>
2022-03-03 23:59:16 +0000

    Add files via upload

---
+++ generator/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+class program
+{
+      
+    static void Main(string[] args)
+    {
+        int [] rnd_size = {10,20,30,40};
+        Random rnd = new Random();
+
+        for(int i = 0; i < rnd_size.Length; i++)
+        {
+
+            string s = "number-gen" + rnd_size[i] + ".txt";
+            
+            StreamWriter sw = new StreamWriter(s);
+            sw.WriteLine(rnd_size[i]);
+            for(int j = 0; j < rnd_size[i]; j++) 
+            {   
+                int r = rnd.Next(0, 100000); 
+                sw.WriteLine(r);
+                
+
+            }
+            sw.Close();
+        }
+        
+    }
+}
